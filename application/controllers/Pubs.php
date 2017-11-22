7e4246675128e7a5bc223a4ddeb678bbb32cb957
Alexander Vegt <alexander@contritio.com>
2017-11-22 04:00:05 +0100

    Merge branch 'master' of https://github.com/Maatwork/BarzoWeb
    # Conflicts:
    #	application/config/routes.php
    #	application/controllers/Login.php
    #	application/controllers/Pubs.php
    #	application/helpers/my_authenticate_helper.php
    #	application/views/login/index.php
    #	css/style.css

--- application/controllers/Pubs.php
+++ application/controllers/Pubs.php
@@ -27,12 +27,16 @@ class Pubs extends CI_Controller {
         $data['menu'] = $this->menulib->getMenuAsArray();
 
         $this->load->model('Pubs_model');
+        $this->load->model('Events_model');
 
         $pubs = $this->Pubs_model->getAllPubs();
         if ($pubs != false) {
             if (isset($pubs) || !empty($pubs)) {
                 $data['pubs'] = $pubs;
             }
+        }
+        if (!empty($events)) {
+            $data['events'] = $events;
         }
 
         $this->load->view('templates/header', $data);
